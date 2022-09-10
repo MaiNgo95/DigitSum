e765c15229b3b42fd2f8545f6c51726fc2d75e29
MaiNgo95 <91213817+MaiNgo95@users.noreply.github.com>
2022-09-10 16:53:29 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+   public static int sumDigit(int number) {
+        if (number < 10) {
+            return -1;
+        }
+        int sum = 0;
+        while (number > 0) {
+
+            //125 /10 = 12 -? 12 * 10 = 120 -> 125- 120 =5 is the last digit
+            int digit = number % 10; //module 125 % 10 = 5 the remain number
+            sum += digit; //add the last digit into the sum
+
+            number /= 10; //number = number/ 10; //125/10 =12
+        }
+
+        return sum;
+
+    }
+
+
+    public static void main(String []args){
+        System.out.println("sum = " + sumDigit(125));
+    }
 }
